c09dc3172a48b4ebcd1b63cbed5468ec8bd61e3a
benney <benneykwag@gmail.com>
2017-10-30 17:09:19 +0900

    update

--- MyApp/app/model/Base.js
+++
@@ -1,7 +0,0 @@
-Ext.define('myapp.model.Base', {
-    extend: 'Ext.data.Model',
-
-    schema: {
-        namespace: 'myapp.model'
-    }
-});
--- MyApp/app/model/Personnel.js
+++
@@ -1,7 +0,0 @@
-Ext.define('myapp.model.Personnel', {
-    extend: 'myapp.model.Base',
-
-    fields: [
-        'name', 'email', 'phone'
-    ]
-});
